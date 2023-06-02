7ab49a7b3a2bb25dc2eba91c82c75695614177c5
Tahoora Mirbeigi <tahoora_m@yahoo.com>
2023-06-02 10:53:16 +0430

    created the configuration information

---
+++ src/main/java/org/example/connection/DBConfig.java
@@ -0,0 +1,7 @@
+package org.example.connection;
+
+public class DBConfig {
+    public static final String URL = "jdbc:postgresql://localhost:5432/stores";
+    public static final String USER_NAME = "postgres";
+    public static final String PASSWORD = "Postgres1234@";
+}
