fbcb38ced9e41a0c72700d25d25f068171d7b051
Tahoora Mirbeigi <tahoora_m@yahoo.com>
2023-06-02 10:54:26 +0430

    created Category Class in entity package

---
+++ src/main/java/org/example/entity/Category.java
@@ -0,0 +1,48 @@
+package org.example.entity;
+
+public class Category {
+    private int categoryID;
+    private String categoryName;
+    private String description;
+
+    public Category() {
+    }
+
+    public Category(String categoryName, String description) {
+        this.categoryName = categoryName;
+        this.description = description;
+    }
+
+    public int getCategoryID() {
+        return categoryID;
+    }
+
+    public void setCategoryID(int categoryID) {
+        this.categoryID = categoryID;
+    }
+
+    public String getCategoryName() {
+        return categoryName;
+    }
+
+    public void setCategoryName(String categoryName) {
+        this.categoryName = categoryName;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @Override
+    public String toString() {
+        return "Category{" +
+                "categoryID=" + categoryID +
+                ", categoryName='" + categoryName + '\'' +
+                ", description='" + description + '\'' +
+                '}';
+    }
+}
