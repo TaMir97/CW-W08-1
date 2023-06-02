e4aa796b9049af4a877004fd3e2a83db4990435e
Tahoora Mirbeigi <tahoora_m@yahoo.com>
2023-06-02 15:28:13 +0430

    creat all validations

---
+++ src/main/java/org/example/validation/Valid.java
@@ -0,0 +1,17 @@
+package org.example.validation;
+
+public class Valid {
+    public static boolean validEmail(String email){
+        String regex = "^(?i)[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,}$";
+        return email.matches(regex);
+
+    }
+
+    public static boolean isWebsite(String website) {
+        String regex = "^(https?://)?([\\da-z.-]+)\\.([a-z.]{2,6})([/\\w .-]*)*/?$";
+        return website.matches(regex);
+    }
+    public static boolean validPassword(String password){
+        return password.matches("^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,10}$");
+    }
+}
