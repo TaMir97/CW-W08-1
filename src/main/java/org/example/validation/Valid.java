e04cdb1b4118bc4af78a7de939f7cd2c20b4244a
Tahoora Mirbeigi <tahoora_m@yahoo.com>
2023-06-03 22:44:12 +0430

    Create the Valid.java Class for checking the validations

--- src/main/java/org/example/validation/Valid.java
+++ src/main/java/org/example/validation/Valid.java
@@ -13,5 +13,6 @@ public class Valid {
     }
     public static boolean validPassword(String password){
         return password.matches("^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,10}$");
+
     }
 }
