6cdd3fb64696b35557d739f556cb4f9a4e49443e
Tahoora Mirbeigi <tahoora_m@yahoo.com>
2023-06-06 21:48:32 +0430

    modified ProductService.java

--- src/main/java/org/example/service/ProductService.java
+++ src/main/java/org/example/service/ProductService.java
@@ -1,4 +1,22 @@
 package org.example.service;
 
+import org.example.entity.Product;
+import org.example.repository.implementation.ProductRepo;
+
 public class ProductService {
+    ProductRepo productRepo = new ProductRepo();
+
+    public void addNewProduct(Product product) {
+        productRepo.addProduct(product);
+    }
+
+
+    public void updateNewProduct(Product product) {
+        productRepo.updateProduct(product);
+    }
+
+    public void deleteNewProduct(int productID) {
+        productRepo.deleteProduct(productID);
+    }
 }
+
