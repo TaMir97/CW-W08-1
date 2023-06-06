31e9e1872fb4e3d0a140f753c4644d741e9ed4e2
Tahoora Mirbeigi <tahoora_m@yahoo.com>
2023-06-06 21:48:08 +0430

    modified CategoryService.java

--- src/main/java/org/example/service/CategoryService.java
+++ src/main/java/org/example/service/CategoryService.java
@@ -1,4 +1,24 @@
 package org.example.service;
 
+import org.example.entity.Category;
+import org.example.repository.implementation.CategoryRepo;
 public class CategoryService {
+    CategoryRepo categoryRepo = new CategoryRepo();
+
+    public void addNewCategory(Category category) {
+        categoryRepo.addCategory(category);
+    }
+
+    public Category[] loadAllCategories() {
+        return categoryRepo.loadAllCategory();
+    }
+
+    public void updateNewCategory(Category category) {
+        categoryRepo.updateCategory(category);
+    }
+
+    public void deleteNewCategory(int categoryID) {
+        categoryRepo.deleteCategory(categoryID);
+    }
 }
+
