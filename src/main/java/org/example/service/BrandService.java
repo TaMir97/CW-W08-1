fa89e2bf70eb904a0d635ac28d88174ffebbd24d
Tahoora Mirbeigi <tahoora_m@yahoo.com>
2023-06-06 21:47:54 +0430

    modified BrandService.java

--- src/main/java/org/example/service/BrandService.java
+++ src/main/java/org/example/service/BrandService.java
@@ -1,18 +1,20 @@
 package org.example.service;
 
 import org.example.entity.Brand;
-import org.example.entity.User;
 import org.example.repository.implementation.BrandRepo;
-import org.example.application.ui.Validation.Valid;
+import org.example.util.Valid;
 
 public class BrandService {
-    UserService userService = new UserService();
     BrandRepo brandRepo= new BrandRepo();
 
-    public void addNewBrand (Brand brand, User user) {
-        if (userService.isExist(user.getUsername()) && Valid.isWebsite(brand.getWebsite())) {
+    public void addNewBrand (Brand brand) {
+        if (Valid.isWebsite(brand.getWebsite())) {
             brandRepo.addBrand(brand);
         }
+    }
+
+    public Brand[] loadAllBrands(){
+        return brandRepo.loadAllBrand();
     }
 
     public void updateNewBrand (Brand brand) {
